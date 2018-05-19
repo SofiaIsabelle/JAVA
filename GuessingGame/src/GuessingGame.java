cf382c7c995d5104247ef456077fc47fb5762598
Nina Rahimi <nina.i.rahimi@gmail.com>
2018-05-18 20:14:36 -0400

    Ask user if they'd like to play again, by integrating a do-while loop

--- GuessingGame/src/GuessingGame.java
+++ GuessingGame/src/GuessingGame.java
@@ -6,25 +6,41 @@ public class GuessingGame {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		// create a random number from 1 - 100 
-		int theNumber = (int)(Math.random()*100+1);
-		// guessing number
-		int guess = 0; 
 		
-		while (guess != theNumber) {
-			System.out.println("Guess a number between 1 and 100.");
-		    //Get the users guess
-			guess = scan.nextInt();
-			
-			if(guess < theNumber)
-				System.out.println(guess + " is too low. Please try again.");
-			else if(guess > theNumber)
-				System.out.println(guess + " is too high. Please try again.");
-			else
-				System.out.println(guess + " is correct! You win!");
-			
-		}
-			
+		//play again String
+		String playAgain = "";
+		
+		//game loop
+		do{
+		
+		
+				// create a random number from 1 - 100 
+				int theNumber = (int)(Math.random()*100+1);
+				// guessing number
+				int guess = 0; 
+				
+				while (guess != theNumber) {
+					System.out.println("Guess a number between 1 and 100.");
+				    //Get the users guess
+					guess = scan.nextInt();
+					
+					if(guess < theNumber)
+						System.out.println(guess + " is too low. Please try again.");
+					else if(guess > theNumber)
+						System.out.println(guess + " is too high. Please try again.");
+					else
+						System.out.println(guess + " is correct! You win!");
+					
+				
+				} // end of while loop for guessing 
+		// ask for 'y' to play again
+				System.out.println(" Would you like to play again (y/n) ? ");
+		        playAgain = scan.next();
+		        
+		 } while(playAgain.equalsIgnoreCase("y"));
+		  System.out.println("Thank you for playing. Goodbye!");
+		
+				
 	}
 
 }
